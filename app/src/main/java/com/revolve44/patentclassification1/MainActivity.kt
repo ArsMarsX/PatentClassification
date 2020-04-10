3de73d28c3fd1324a61859348b8823112151cd2c
ArsMars <tag_mahal@mail.ru>
2020-04-10 17:00:15 +0300

    start! :rocket:

---
+++ app/src/main/java/com/revolve44/patentclassification1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.revolve44.patentclassification1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a= "privet"
+
+    }
+}
