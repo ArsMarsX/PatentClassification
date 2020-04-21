e5bfa5d10fab698c903335dcd29f317bbe3f2253
ArsMars <tag_mahal@mail.ru>
2020-04-21 16:39:06 +0300

    added recyclerview

---
+++ app/src/main/java/com/revolve44/patentclassification1/ExampleItem.kt
@@ -0,0 +1,4 @@
+package com.revolve44.patentclassification1
+
+
+data class ExampleItem(val imageResource: Int, val text1: String, val text2: String)
--- app/src/main/java/com/revolve44/patentclassification1/MainActivity.kt
+++ app/src/main/java/com/revolve44/patentclassification1/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.revolve44.patentclassification1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a= "privet"
 
+        val exampleList = generateDummyList(500)
+
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_autorenew_black_24dp
+                1 -> R.drawable.ic_beach_access_black_24dp
+                else -> R.drawable.ic_child_care_black_24dp
+            }
+
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+
+        return list
     }
 }
